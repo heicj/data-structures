fa6f9ae3fc5580314272c581572bf21de6054f50
Charlie Heiner <heinercj@gmail.com>
2018-02-01 08:58:05 -0800

    single linked list with search, remove, length

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # data-structures
+
+This repository has sample code for differect classic data structures.  Implemented with python. 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name= "trigrams"
+	description = "this is for a number of different data structures"
+	version = 0.1
+	author = "Charlie Heiner"
+	license = 'MIT'
+	install_requires = []
+)
